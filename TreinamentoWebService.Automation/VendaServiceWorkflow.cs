7ee43d6862589d680ce820cd84227805cb9a79d3
thiagol <thiago.m.lima@outlook.com>
2018-01-31 13:57:59 -0200

    Added VendaService tests

---
+++ TreinamentoWebService.Automation/VendaServiceWorkflow.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Xml;
+using TreinamentoWebService.Automation.VendaService;
+
+namespace TreinamentoWebService.Automation
+{
+    public class VendaServiceWorkflow
+    {
+        private VendaServiceClient client;
+
+        public VendaServiceWorkflow()
+        {
+            client = new VendaServiceClient();
+        }
+
+        public string GetCodigoVerificacao(string numeroNotaFiscal )
+        {
+            NfseReponse response = client.GetNfse( numeroNotaFiscal );
+            return response.Nfse.CodigoVerificacao;
+        }
+
+        public DateTime GetDataEmissao( string numeroNotaFiscal )
+        {
+            return client.GetNfse( numeroNotaFiscal ).Nfse.DataEmissao;
+        }
+
+        public string GetVendaServicoDiscriminacao(int idVenda)
+        {
+            string nfse = client.GetVenda( idVenda ).Nfse;
+            XmlDocument nfseXml = new XmlDocument();
+            nfseXml.LoadXml( nfse );
+            XmlNode node = nfseXml.SelectSingleNode( "//Discriminacao" );
+            return node.InnerText;
+        }
+    }
+}
--- TreinamentoWebService.Test/ServiceTests.cs
+++ TreinamentoWebService.Test/ServiceTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Xml;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using TreinamentoWebService.Automation;
 
@@ -7,11 +8,45 @@ namespace TreinamentoWebService.Test
     [TestClass]
     public class ServiceTests
     {
-        [TestMethod]
+        private VendaServiceWorkflow vendaServiceWorkflow;
+        private HelloServiceWorkflow helloServiceWorkflow;
+
+        public ServiceTests()
+        {
+            helloServiceWorkflow = new HelloServiceWorkflow();
+            vendaServiceWorkflow = new VendaServiceWorkflow();
+        }
+
+        [ TestMethod]
         public void ValidateHelloMessage()
         {
-            HelloServiceWorkflow helloServiceWorkflow = new HelloServiceWorkflow();
             Assert.IsTrue( helloServiceWorkflow.VerifiyHelloMessage( "Thiago" ) );
+        }
+
+        [TestMethod]
+        public void ValidaCampoCodigoVerificacao()
+        {
+            const string CodigoEsperado = "da84";
+            Assert.AreEqual( vendaServiceWorkflow.GetCodigoVerificacao( GetNumeroNotaFiscal() ), CodigoEsperado );
+        }
+
+        [TestMethod]
+        public void ValidaCampoDataEmissao()
+        {
+            Assert.IsTrue( vendaServiceWorkflow.GetDataEmissao( GetNumeroNotaFiscal() ) > new DateTime( 2018, 1, 1 ) );
+        }
+
+        [TestMethod]
+        public void ValidaCampoDiscriminacao()
+        {
+            const int VendaId = 1;
+            const string ExpectedDiscriminacao = "Teclado T2";
+            Assert.AreEqual( vendaServiceWorkflow.GetVendaServicoDiscriminacao( VendaId ), ExpectedDiscriminacao );
+        }
+
+        private string GetNumeroNotaFiscal()
+        {
+            return "2018";
         }
     }
 }
